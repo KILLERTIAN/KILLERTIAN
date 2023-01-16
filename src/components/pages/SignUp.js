f9a377b7b505c0a7b624cd7bc4e5dfaf7ad4bf2b
Om Sharma <omsharma050322@gmail.com>
2023-01-16 23:16:49 +0530

    Minor updates in Signup

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './SignUp.css';
-// import styled from "styled-components"
-// import {FaGoogle,FaFacebook,FaTwitter} from "react-icons/fa"
 
-// export default function SignUp() {
-//   return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
-// }
 function SignUp() {
   return (
     <body className='body'>
       <div className="MainContainer">
-        
+        <h2 className="welcometext">WELCOME</h2>
+        <input type="email" placeholder='Enter your email' />
+        <input type="password" placeholder='Enter your password' />
+        <button className = "signup">Sign Up</button>
+        <h4 className="or">or Login with</h4>
+        <div className="divider"></div>
+        {/* <div className="social">
+          <img src="https://icons8.com/icon/17949/google" alt="google" className="google" />
+        </div> */}
       </div>
     </body>
     
   );
 }
+
 
 export default SignUp;
