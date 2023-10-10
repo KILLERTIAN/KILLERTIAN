0e983e6ebc827640ca4accce9ffaeae05351dc98
Om Sharma <omsharma050322@gmail.com>
2023-10-10 22:44:06 +0530

    Deployed Version out

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ function Footer() {
               type='email'
               placeholder='Your Email'
             />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
+            <Button buttonStyle='btn--primary'>Subscribe</Button>
           </form>
         </div>
       </section>
